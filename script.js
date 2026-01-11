e4fd5af89c9bdeb806f9d9305d2046cfafeecf26
fra28_8 <franciscocarreno1502@gmail.com>
2026-01-11 00:42:36 -0300

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    console.log("Sistema Euforia Team's Online");
-    // Efecto simple de aparición
-    document.querySelector('.hero').style.opacity = 0;
-    setTimeout(() => {
-        document.querySelector('.hero').style.transition = "opacity 2s";
-        document.querySelector('.hero').style.opacity = 1;
-    }, 100);
-});
